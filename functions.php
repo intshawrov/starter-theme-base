3fc2df0a13eb3bd93cfbc99db45e7dfa054c128a
Shawrov <intshawrov@gmail.com>
2026-01-15 01:36:45 +0600

    Updated

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ add_action("wp_enqueue_scripts", "mir_enqueue_scripts");
 function mir_theme_support(){
       add_theme_support('title-tag');
       add_theme_support('post-thumbnails');
+      
 
       add_theme_support("menus"); 
 
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
 
-<h1>Hello World</h1>
+<div class="container">
+      <div class="row">
+            <div class="col-md-12"><h1>Title</h1></div>
+            <div class="col-md-12"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/image1.jpg" alt=""></div>
+      </div>
+</div>
 
 
 
