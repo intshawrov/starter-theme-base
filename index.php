7def69b49a7448eefa4850251c536801a5d86956
Shawrov <intshawrov@gmail.com>
2026-01-18 23:47:15 +0600

    Updated

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <div class="container">
       <div class="row">
             <div class="col-md-12"><h1>Title</h1></div>
-            
+            <?php if( get_theme_mod('body_image')): ?>
             <div class="col-md-12"><img src="<?php echo esc_url( get_theme_mod('body_image') ); ?>" alt=""></div>
       </div>
 </div>
 
 
 
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
